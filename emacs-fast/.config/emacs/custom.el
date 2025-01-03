8bb037e6c17e19e560360112495d47979b1a14a6
makisat <makiya28sato@gmail.com>
2025-01-03 02:46:33 -0500

    minimum emacs

---
+++ emacs-fast/.config/emacs/custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" default))
+ '(delete-selection-mode nil)
+ '(whitespace-display-mappings
+   '((space-mark 32
+                 [183]
+                 [46])
+     (space-mark 160
+                 [164]
+                 [95])
+     (newline-mark 10
+                   [8626 10])
+     (tab-mark 9
+               [187 9]
+               [92 9]))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-done ((t (:background "gray30" :foreground "white" :weight bold))))
+ '(org-todo ((t (:background "light green" :foreground "black" :weight bold)))))
